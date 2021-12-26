70ce364fa2db0c9cf0ecdae30e66c0bd56c7c5d2
Mouhcine Daali <daalim277@gmail.com>
2021-12-26 13:34:56 +0100

    add js to detect if the input is valid

--- contact/script.js
+++ contact/script.js
@@ -2,3 +2,23 @@ let mobilMenu = () => {
     document.querySelector("nav").classList.toggle("menu-on");
 }
 
+let contactName = document.querySelector("#name"),
+    contactEmail = document.querySelector("#email"),
+    contactSubject = document.querySelector("#subject"),
+    contactMessage = document.querySelector("#message");
+
+let errorInput = document.querySelector(".alert");
+
+let alertContact = () => {
+    if(contactName.value == "" || contactEmail.value == "" || contactSubject == "" || contactMessage == "") {
+        errorInput.classList.add("on");
+        setTimeout(() => {errorInput.classList.remove("on")}, 3000);
+    } else {
+        window.alert(`  your name is : ${contactName.value}
+        and your email is : ${contactEmail.value}
+        and your subject is : ${contactSubject.value}
+        and your message is : ${contactMessage.value}`);
+    }
+
+}
+
